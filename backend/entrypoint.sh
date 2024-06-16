a3fa16491a749e6f8047c4374840c2d3a4a2e22d
yuki <nagase.yuki0123@gmail.com>
2024-06-16 17:12:21 +0900

    [fix] fix rails config

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,4 +3,9 @@ set -e
 
 rm -f /api/tmp/pids/server.pid
 
+bundle exec rails db:create # 初回起動時のみ
+bundle exec rails db:migrate
+bundle exec rails db:seed
+bundle exec rails s -p 3001 -b '0.0.0.0'
+
 exec "$@"
